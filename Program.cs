72ec43fb08c6bfeac09e1940ce69da36af50dc65
trinebra <157688079+trinebra@users.noreply.github.com>
2024-09-04 09:37:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+Main();
+
+
+
+void Main()
+{
+    Console.WriteLine("Welcome to the Code Project Generator");
+    Console.WriteLine("You will find a list of different projects you can dive into, or you can get a random one from the generator!");
+
+
+
+
+
+
+}
